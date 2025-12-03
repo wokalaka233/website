83271d0b4a44300a4eb7b38442376cb0626db570
wokalaka233 <860992714@qq.com>
2025-12-04 00:39:31 +0800

    feat: Improve index.html and vite.config.ts
    Updates index.html with mobile viewport meta tag and custom animations.
    Adds Tailwind CSS and adjusts scrollbar styling.
    Configures Vite build output directory.

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // 因为你绑定了域名 wokalaka.top，所以这里必须是 '/'
   base: '/', 
+  build: {
+    outDir: 'dist',
+  }
 })
