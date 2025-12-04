edf1f1361196d8a32de40ba324ba194d920d1ff6
wokalaka233 <860992714@qq.com>
2025-12-04 20:33:38 +0800

    重上传

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   // 因为你绑定了域名 wokalaka.top，所以这里必须是 '/'
+  // 如果未来取消了域名绑定，需要改为 '/wokalaka233.github.io/'
   base: '/', 
   build: {
     outDir: 'dist',
