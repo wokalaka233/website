cdeff7d1e886c6dd9352d85c546833470de084aa
wokalaka233 <860992714@qq.com>
2025-12-04 21:47:04 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   // 因为你绑定了域名 wokalaka.top，所以这里必须是 '/'
   // 如果未来取消了域名绑定，需要改为 '/wokalaka233.github.io/'
-  base: '/', 
+  base: './', 
   build: {
     outDir: 'dist',
   }
